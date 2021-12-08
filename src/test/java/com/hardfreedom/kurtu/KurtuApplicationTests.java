98ce706100f008e29c22b8c513317aae4270d20e
Martynas Sklizmantas <saint@ghost.lt>
2021-12-08 21:32:32 +0200

    initial commit

---
+++ src/test/java/com/hardfreedom/kurtu/KurtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardfreedom.kurtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
